348c66e294e6d9db9c16068763822d31e00016f2
mamadou adiouma <adiou.xyz@gmail.com>
2025-01-26 00:08:51 +0000

    Updated UI and all Code.

--- script.js
+++ script.js
@@ -1,93 +1,100 @@
 "use strict"
-const block_access = (e) => {
-     if (e.keyCode == 123 || (e.ctrlKey && e.shiftKey && e.keyCode == 74) || (e.ctrlKey && e.keyCode == 85)) {
-          e.preventDefault()
-          alert("Interdit ! 😁")
-          window.location = "https://github.com/"
-     }
-}
-
-
-const input_value = document.getElementById("moy_input")
+const input = document.getElementById("input")
 const calc_btn = document.getElementById("calc_btn")
-const moy_gen = document.getElementById("moy_gen")
-const article = document.querySelector("article")
-
-// Regex de 3 notes (decimals ou entiers), séparées par des virgules
-// const is_valid_format_and_numeric = /^(\d+(\.\d+)?\s*,\s*\d+(\.\d+)?\s*,\s*\d+(\.\d+)?)$/
+const mssgErr = document.getElementById("display_err")
+const aside = document.querySelector("aside")
 
 // Regex de 6 notes (decimals ou entiers), séparées par des virgules
 const is_valid_format_and_numeric = /^(\d+(\.\d{1,2})?\s*,\s*){5}\d+(\.\d{1,2})?$/
 
 // Regex qui n'accepte pas des caracteres
-const are_chars = (input_val) => /[^0-9,.\s]/.test(input_val);
+const are_chars = (input_value) => /[^0-9,.\s]/.test(input_value);
 
-const clear_input = () => {
-     input_value.value = ""
-     input_value.focus()
+const clear_and_focus = () => {
+     input.value = ""
+     // input.focus()
 }
 
-const check_verification = () => {
-     const inf0_sup20 = (moy) => !isNaN(moy) && moy >= 0 && moy <= 20
-     const are_numbers = input_value.value
-          .trim()
-          .split(',')
-          .map((moy) => Number(moy))
-          .every(inf0_sup20)
+const showMssgErr = () => {
+     mssgErr.classList.add("active")
+}
+const hideMssgErr = () => {
+     mssgErr.classList.remove("active")
+}
 
-     if (!(input_value.value.trim() === "")) {
-          if (are_numbers && input_value.value.match(is_valid_format_and_numeric) !== null) {
+const verification = () => {
+
+     if (!(input.value.trim() === "")) {
+          if (are_chars(input.value)) {
+               clear_and_focus()
+               showMssgErr()
+               mssgErr.textContent = "❌ Les caracteres ne sont pas autisés ! uniquement des données valides (notes), séparées par des virgules(,)."
+               return false
+          }
+
+          if (input.value.match(is_valid_format_and_numeric) === null) {
+               showMssgErr()
+               input.focus()
+               mssgErr.textContent = "⚠️Le champs attend exactement 6 moyennes (Semestre 1-6)."
+               return false
+          }
+
+          if (input.value.match(is_valid_format_and_numeric) !== null && !are_chars(input.value)) {
                try {
-                    const arr_moy_to_numbers = input_value.value.split(',').map((moy) => Number(moy))
-                    console.log(arr_moy_to_numbers.length);
+                    const moyennes_arr_numbers = input.value.split(',').map((moy) => Number(moy))
+                    const inf0_sup20 = moyennes_arr_numbers.map((moy) => moy).every((moy) => !isNaN(moy) && moy >= 0 && moy <= 20)
 
-                    if (arr_moy_to_numbers.length === 6) {
-                         console.log(arr_moy_to_numbers);
-                         arr_moy_to_numbers.forEach((moy, index) => {
-                              article.innerHTML +=
-                                   `
-                              <div class="note_box">
-                                   <span>S${index + 1}</span>
-                                   <h4 class="${moy < 10 ? 'sub_moy' : 'moy'}">${moy < 10 ? '0' + moy : moy}</h4>
-                              </div>
-                             `
-                         })
-                         clear_input()
-
-                         moy_gen.innerText = `${(arr_moy_to_numbers.reduce((acc, curr) => acc + curr, 0) / arr_moy_to_numbers.length).toFixed(2)} / 20`
-                         document.querySelector(".good_luck").classList.add("active")
+                    if (!inf0_sup20) {
+                         showMssgErr()
+                         input.focus()
+                         mssgErr.textContent = "⚠️Les moyennes doivent être comprises entre [0-20]"
+                         return false
                     } else {
-                         alert("Le champs attend exactement 6 moyennes (Semestre 1-6)👌.")
-                         document.querySelector(".good_luck").classList.remove("active")
+                         const moy_gene = `${(moyennes_arr_numbers.reduce((acc, curr) => acc + curr, 0) / moyennes_arr_numbers.length).toFixed(2)} / 20`
+                         const ul = document.createElement("ul")
+                         moyennes_arr_numbers.forEach((moy, index) => {
+                              const li = document.createElement("li")
+                              li.classList.add("note_box")
+                              li.innerHTML +=
+                                   `
+                                   <span>S${index + 1}</span>
+                                   <h2 class="${moy < 10 ? 'sub_moy' : 'moy'}">${moy < 10 ? '0' + moy : moy}</h2>
+                              `
+                              ul.appendChild(li)
+                         })
+                         const h3 = document.createElement("h3")
+                         h3.innerHTML = `La moyenne générale est : <strong>${moy_gene}</strong>`
+                         aside.appendChild(ul)
+                         aside.appendChild(h3)
+                         console.log(moy_gene);
                     }
 
                } catch (error) {
-                    console.log(error)
+                    console.log(error);
                }
-
-          } else if (are_chars(input_value.value)) {
-               alert("Veuillez entrer des données valides (notes), séparées par des virgules 😒!")
-               clear_input()
-               return false
           }
+
+          clear_and_focus()
+
      } else {
-          alert("Champs vide - Veuillez remplir le champ input 😉!")
-          article.innerHTML = " "
-          clear_input()
+          clear_and_focus()
+          showMssgErr()
+          mssgErr.textContent = "⚠️ Le Champ est requis !"
+          return false
      }
+
 }
 
+const reset = () => {
+     hideMssgErr()
+     aside.innerHTML = ""
+}
 
-calc_btn.addEventListener("click", check_verification)
-input_value.addEventListener("keydown", () => {
-     article.innerHTML = ""
-     moy_gen.innerText = "---"
-     document.querySelector(".good_luck").classList.remove("active")
-
+calc_btn.addEventListener('click', verification)
+document.addEventListener("keydown", (e) => {
+     if (e.key === "Enter") {
+          verification()
+     }
 })
-document.addEventListener("keydown", block_access)
-document.addEventListener("contextmenu", e => {
-     e.preventDefault()
-     alert("Interdit 😁!")
-     window.location = "https://github.com/"
-})
+document.addEventListener("DOMContentLoaded", clear_and_focus)
+input.addEventListener("keydown", reset)
